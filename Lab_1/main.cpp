ddaf92c37b5df936b4a2f951d22f45d74477bc6b
user <viktoriacobko@gmail.com>
2022-09-27 14:52:19 +0300

    var4

--- Lab_1/main.cpp
+++ Lab_1/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main () {
-    cout << "Hello, World!" << endl;
+    // Оголошення змінних x, y.
+    float x, y;
+    // Введення x, y.
+    cout << "Enter x: \n";
+    cin >> x;
+    if (!x){
+        cout << "Incorect input\n";
+    }
+    else {
+        cout << "Enter y: \n";
+        cin >> y;
+        if (!y) {
+            cout << "Incorect input\n";
+        }
+        else {
+            // Перевірка умови задачі: |y|>0=|x|, |y|<=1.
+
+            if (fabs(y) >= fabs(x)) {
+                fabs(y) <= 1 ? cout << "The point falls into the region.\n" : cout
+                        << "The point does not fall into the region.\n";
+            } else {
+                cout << "The point does not fall into the region.\n";
+
+            }
+        }
+
+    }
+
 }
