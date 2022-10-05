50589254d3f1d53019d52225db34841e795a8827
user <viktoriacobko@gmail.com>
2022-10-05 16:14:21 +0300

    var4

--- Lab_2/main.cpp
+++ Lab_2/main.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <cmath>
 
+
 using namespace std;
 
 int main () {
+    cout << "Lab_2\nvar_4\n";
 
-    long n = 0;
+    double n = 1.0;
     double curr;
-    double sum = 0;
-    int k = 1;
+    double sum = 0.0;
+    double k = 1.0;
     const double eps = 0.000001;
-    for ( n < 10; n++;){
+    curr = k * ( n*n + 1.0 ) / ( n*n*n + 3.0 );
 
-        curr = k * ( n*n + 1 ) / ( n*n*n + 3 );
-        if (fabs(curr) >= eps){
-            cout << sum;
-            break;
-        }
-        else {
-            sum += curr;
-            if (n=10){
-                cout << sum;
-            }
-            else{
+    while (fabs(curr) >= eps){
 
-            }
-            k = -1 * k;
-            cout << k;
+        n = n + 1.0;
+        curr = k * ( n*n + 1.0 ) / ( n*n*n + 3.0 );
+        k=-k;
+        sum = sum + curr;
+        if(n == 9.0) {
+            cout << "The sum of 10 members of the series: \n" << sum << "\n";
         }
 
     }
+    cout << "The total sum of the series: \n" << sum << "\n";
 
+
+    //return 0;
 }
