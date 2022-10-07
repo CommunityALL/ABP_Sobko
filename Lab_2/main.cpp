e2ebad8339eb51879ba399d1427501f289a28713
user <viktoriacobko@gmail.com>
2022-10-07 10:43:33 +0300

    var4

--- Lab_2/main.cpp
+++ Lab_2/main.cpp
@@ -7,12 +7,13 @@ using namespace std;
 int main () {
     cout << "Lab_2\nvar_4\n";
 
-    double n = 1.0;
+    double n = 0.0;
     double curr;
     double sum = 0.0;
     double k = 1.0;
     const double eps = 0.000001;
     curr = k * ( n*n + 1.0 ) / ( n*n*n + 3.0 );
+    sum = sum + curr;
 
     while (fabs(curr) >= eps){
 
@@ -23,9 +24,12 @@ int main () {
         if(n == 9.0) {
             cout << "The sum of 10 members of the series: \n" << sum << "\n";
         }
+//        if (n == 10.0) {
+//            cout << "The sum of 11 members of the series: \n" << sum << "\n";
+//            cout << curr << "\n";
+//        }
 
     }
     cout << "The total sum of the series: \n" << sum << "\n";
-
 
 }
