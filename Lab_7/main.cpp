d057455c6b4eee968ff7f5b3eb1b087004907058
user <viktoriacobko@gmail.com>
2022-11-08 11:09:38 +0200

    var4

--- Lab_7/main.cpp
+++ Lab_7/main.cpp
@@ -41,35 +41,73 @@ int main(){
     for (int i = 0; i < lines-1;){
         unsigned long long min;
         string minString, maxString;
-        int equality = 0;
+        char minType, maxType;
+        int minQuan, maxQuan;
+        int minWeight, maxWeight;
+        int equality1 = 0;
+        int equality2 = 0;
         if (ArrayNames[i].size() < ArrayNames[i+1].size()){
             min = ArrayNames[i].size();
             minString = ArrayNames[i];
             maxString = ArrayNames[i+1];
+            minType = type[i];
+            minQuan = quan[i];
+            minWeight = weight[i];
+            maxType = type[i+1];
+            maxQuan = quan[i+1];
+            maxWeight = weight[i+1];
         }
         if (ArrayNames[i].size() == ArrayNames[i+1].size()){
             min = ArrayNames[i].size();
             minString = ArrayNames[i];
             maxString = ArrayNames[i+1];
+            minType = type[i];
+            minQuan = quan[i];
+            minWeight = weight[i];
+            maxType = type[i+1];
+            maxQuan = quan[i+1];
+            maxWeight = weight[i+1];
         }
         if (ArrayNames[i].size() > ArrayNames[i+1].size()){
             min = ArrayNames[i+1].size();
             minString = ArrayNames[i+1];
             maxString = ArrayNames[i];
+            minType = type[i+1];
+            minQuan = quan[i+1];
+            minWeight = weight[i+1];
+            maxType = type[i];
+            maxQuan = quan[i];
+            maxWeight = weight[i];
         }
         for (int j = 0; j < min;){
-            if (ArrayNames[i][j] > ArrayNames[i+1][j]) {
-                string variable = ArrayNames[i];
+            if (ArrayNames[i][j] > ArrayNames[i+1][j] && equality1 == j) {
+                string variable1 = ArrayNames[i];
                 ArrayNames[i] = ArrayNames[i + 1];
-                ArrayNames[i + 1] = variable;
+                ArrayNames[i + 1] = variable1;
+                char variable2 = type[i];
+                type[i] = type[i+1];
+                type[i+1] = variable2;
+                int variable3 = quan[i];
+                quan[i] = quan[i+1];
+                quan[i+1] = variable3;
+                int variable4 = weight[i];
+                weight[i] = weight[i+1];
+                weight[i+1] = variable4;
+                equality1++;
             }
-            if (ArrayNames[i][j] == ArrayNames[i+1][j] && equality == j){
+            if (ArrayNames[i][j] == ArrayNames[i+1][j] && equality2 == j){
                 if (ArrayNames[i][min-1] == ArrayNames[i+1][min-1]){
                     string variable = ArrayNames[i];
                     ArrayNames[i] = minString;
                     ArrayNames[i + 1] = maxString;
+                    type[i] = minType;
+                    quan[i] = minQuan;
+                    weight[i] = minWeight;
+                    type[i+1] = maxType;
+                    quan[i+1] = maxQuan;
+                    weight[i+1] = maxWeight;
                 }
-                equality++;
+                equality2++;
             }
             j++;
         }
